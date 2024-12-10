852167387a2b1bbbffc7f3486ca045b1ccf527cc
Neeku <59068472+itsneeku@users.noreply.github.com>
2024-12-09 21:58:20 -0500

    add shellHook.welcome and shellHook.listPkgs

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Small tools for a sweeter Nix Flakes experience";
+  
+  outputs = { self }: {
+    shellHook = {
+      welcome = ''
+        echo -e "Entering dev shell for $(basename "$(pwd)")..."
+      '';
+      listPkgs = ''
+        echo "Installed nix packages:"
+        for path in ${builtins.concatStringsSep " " packages}; do
+          pkg=$(basename "$path" | awk -F '-' '{print substr($0, index($0, $2))}')
+          echo -e "\t$pkg"
+        done
+        echo
+      ''
+    };
+  };
+}
