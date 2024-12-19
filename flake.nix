8cd26ba581a9984a8c4308c52a2bd6f01182e153
Neeku <59068472+itsneeku@users.noreply.github.com>
2024-12-18 19:18:32 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,18 @@
 {
   description = "Small tools for sweeter flakes";
 
-  outputs =
-    { ... }:
-    {
-      welcome = packages: ''
-        echo -e "\nEntering dev shell for $(basename "$(pwd)")..."
-        echo "Installed nix packages:"
-        for path in ${builtins.concatStringsSep " " packages}; do
-          pkg=$(basename "$path" | awk -F '-' '{print substr($0, index($0, $2))}')
-          echo -e "\t$pkg"
-        done
-        echo
-      '';
-    };
+  outputs = {
+    welcome = ''
+      printf "
+      🔨 Entering Shell for $(basename "$(pwd)")
+
+      📦 Installed nix packages:
+      $(for path in ${builtins.concatStringsSep " " (map (pkg: "${pkg}") devDeps)}; do
+        pkg=$(basename "$path" | awk -F '-' '{print substr($0, index($0, $2))}')
+        echo -e "\t$pkg"
+      done)
+
+      "
+    '';
+  };
 }
